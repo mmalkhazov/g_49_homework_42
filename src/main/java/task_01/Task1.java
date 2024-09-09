879281502e1b47879ab3fc9d308a4378dad0e1bf
mmalkhazov <mmalkhazov@yahoo.com>
2024-09-09 14:55:43 +0200

    task_01_update

--- src/main/java/task_01/Task1.java
+++ src/main/java/task_01/Task1.java
@@ -2,6 +2,6 @@ package task_01;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Task1 of the task_01.");
+        System.out.println("updated Task1 of the task_01.");
     }
 }
