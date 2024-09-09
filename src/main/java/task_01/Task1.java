d9e850297c437f83e4f69c62ca8ffa41e8da538f
mmalkhazov <mmalkhazov@yahoo.com>
2024-09-09 14:10:05 +0200

    task_01

---
+++ src/main/java/task_01/Task1.java
@@ -0,0 +1,7 @@
+package task_01;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Task1 of the task_01.");
+    }
+}
---
+++ src/main/java/task_01/Task2.java
@@ -0,0 +1,7 @@
+package task_01;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("Task2 of the task_01.");
+    }
+}
